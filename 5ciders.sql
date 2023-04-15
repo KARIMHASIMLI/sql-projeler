e3c1402c267167a90442e4cdb4bfc9eb81c60eb1
Kerim <Karim Hasimli>
2023-04-15 22:54:39 +0400

    5 ci proje

---
+++ 5ciders.sql
@@ -0,0 +1,15 @@
+select title, lenght from film
+where title like '%n'
+order by lengh DESC
+limit 5;
+
+select title, lenght from film
+where title like '%n'
+order by lengh ASC 
+OFFSET 5
+limit 5;
+
+select last_name, store_id from customer
+where store_id = 1
+order by last_name DESC
+limit 4;
