02c63d39c623c35ec64fb75da33c541e69b04cc6
Kerim <Karim Hasimli>
2023-04-15 15:40:33 +0400

    3cu proje

---
+++ 3cuders.sql
@@ -0,0 +1,13 @@
+select country from country
+where country LIKE 'A%a';
+
+select country from country
+where country Like ______n;
+
+select title from film
+where title ILIKE '%t%t%t%t';
+
+select * from film
+where title LIKE 'C%' 
+and lenght > 90
+and rental_rate = 2.99
